bde4f9aceda90484e17b8bd44fb98e0dd30b25da
Salman K <Salman.K1@BRILLIO.COM>
2025-11-05 00:23:25 +0530

    Initialize backend and frontend with basic configurations and structure

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className='text-3xl text-red-300 font-bold underline'>NoteZilla</div>
+    </div>
+  )
+}
+
+export default App;
