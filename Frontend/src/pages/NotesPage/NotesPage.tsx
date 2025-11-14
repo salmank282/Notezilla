2558b7da8763d32e78a0f2200627b304743363ad
Salman K <Salman.K1@BRILLIO.COM>
2025-11-15 01:53:24 +0530

    Project structure & Add routing and layout components for the frontend application

---
+++ Frontend/src/pages/NotesPage/NotesPage.tsx
@@ -0,0 +1,14 @@
+/**
+ *  @dependencies
+ */
+import React from 'react'
+
+const NotesPage:React.FC = () => {
+  return (
+    <div>
+      <h1>NotesPage</h1>
+    </div>
+  )
+}
+
+export default NotesPage;
---
+++ Frontend/src/pages/TrashPage/TrashPage.tsx
@@ -0,0 +1,14 @@
+/**
+ *  @dependencies
+ */
+import React from "react";
+
+const TrashPage: React.FC = () => {
+  return (
+    <div>
+      <h1>TrashPage</h1>
+    </div>
+  );
+};
+
+export default TrashPage;
