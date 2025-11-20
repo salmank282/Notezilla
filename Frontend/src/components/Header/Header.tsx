052980b2d60d8b4ee30565f3ee323f9417f4ab63
Salman K <Salman.K1@BRILLIO.COM>
2025-11-21 00:40:27 +0530

    project structure,header,sidebar are done

--- Frontend/src/components/Header/Header.tsx
+++ Frontend/src/components/Header/Header.tsx
@@ -1,11 +1,65 @@
-import React from 'react'
+/**
+ * The `Header` component represents the top section of the application,
+ * containing the application title, a search bar, and a user avatar.
+ * @component
+ * @returns {JSX.Element} The rendered Header component.
+ * @author Salman <salmankamalbasha@gmail.com>
+ */
 
-function Header() {
+/**
+ *  @dependencies
+ */
+import React from "react";
+
+/**
+ * Styles
+ */
+import "./Header.css";
+
+/**
+ * icons
+ */
+import notes_icon from "../../assets/images/notes_icon.png";
+import avatar from "../../assets/images/avatar.jpg";
+import { IoSearch } from "react-icons/io5";
+
+/**
+ * helpers
+ */
+import NoteZillaStringHelper from "../../utils/StringHelper";
+
+const Header: React.FC = () => {
+  const { title } = NoteZillaStringHelper;
+
   return (
-    <div className='text-3xl text-amber-700'>
-      Header Demo
+    <div className="header-container">
+      <div className="flex gap-2 items-center">
+        <img
+          src={notes_icon}
+          className="rounded-[10px]"
+          alt="notes-icon"
+          height={"34px"}
+          width={"34px"}
+        />
+        <div className="satisfy-regular text-white text-[30px]">
+          {title.noteZilla}
+        </div>
+      </div>
+      <div className="Search-container">
+        <div className="absolute top-1 left-1">
+          <IoSearch className=" h-[22px] w-[22px] text-[#323232]" />
+        </div>
+        <input type="text" className="search" />
+      </div>
+      <img
+        src={avatar}
+        className="rounded-full"
+        alt="avatar"
+        height={"34px"}
+        width={"34px"}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ Frontend/src/pages/PersonalPage/PersonalPage.tsx
@@ -0,0 +1,14 @@
+/**
+ *  @dependencies
+ */
+import React from 'react'
+
+const PersonalPage:React.FC = () => {
+  return (
+    <div>
+        <h1>PersonalPage</h1>
+    </div>
+  )
+}
+
+export default PersonalPage;
